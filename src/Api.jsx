079e836f8154776cc7d3f6766d08a816cd3800c5
Chamith-Digitus <chamith.h@digitustecglobal.com>
2025-02-27 17:05:02 +0530

    bug fixed

--- src/Api.jsx
+++ src/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export class Api {
-  serverUrl = "http://127.0.0.1:3000/";
+  serverUrl = "https://syneris-backend.vercel.app/";
 
   async contact(data) {
     return await axios.post(this.serverUrl + "contact/direct", data);
