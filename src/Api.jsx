469f579fa76244755eff2641db3a97ccf101d530
Chamith-Digitus <chamith.h@digitustecglobal.com>
2025-02-28 05:51:10 +0530

    api changes

--- src/Api.jsx
+++ src/Api.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export class Api {
-  // serverUrl = "https://syneris-backend.vercel.app/";
-  serverUrl = "http://127.0.0.1:9569/";
+  serverUrl = "https://syneris-backend.vercel.app/";
+  // serverUrl = "http://127.0.0.1:9569/";
 
   async contact(data) {
     return await axios.post(this.serverUrl + "contact/direct", data);
