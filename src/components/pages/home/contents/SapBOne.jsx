e4649d8d3a27860e1f47f15b558c66732b32d9ad
Chamith-Digitus <chamith.h@digitustecglobal.com>
2025-03-02 11:10:03 +0530

    new changes

--- src/components/pages/home/contents/SapBOne.jsx
+++ src/components/pages/home/contents/SapBOne.jsx
@@ -1,30 +1,44 @@
 import "../../../../styles/pages/home/contents/SapBOne.css";
 import sapB1 from "../../../../assets/images/home/sapB1.png";
+import b1Img from "../../../../assets/images/sap/b1Home.png";
 
 export const SAPBOne = () => {
   return (
-    <div className="SapBOne px-4 py-4 px-md-5 py-md-5">
+    <div className="SapBOne px-4 pt-4 px-md-5 pt-md-5">
       <div className="py-4">
         <p className="mb-0 info-tag">SAP BUSINESS ONE</p>
-        <h3 className="main-t-all">THE COMPLETE ERP SOLUTION FOR GROWING BUSINESSES</h3>
+        <h3 className="main-t-all">
+          THE COMPLETE ERP SOLUTION FOR GROWING BUSINESSES
+        </h3>
 
-        <p className="sap-description">
-          SAP Business One is a comprehensive, integrated ERP solution designed
-          to streamline operations, improve decision-making, and drive growth
-          for small and medium-sized businesses. Whether you're managing
-          inventory, finances, or production, SAP Business One provides the
-          tools you need to take your business to the next level.
-        </p>
-
-        <div className="d-flex w-100 flex-column align-items-between flex-md-row justify-content-md-between align-items-md-end mt-4">
-          <div className="d-flex align-items-center">
-            <img className="b1Img" src={sapB1} alt="sapb1" />
+        <div className="d-lg-flex mt-4 pt-2">
+          <div className="d-flex justify-content-center justify-cotent-lg-start">
+            <img src={b1Img} className="b1ImgHome" alt="" />
           </div>
 
-          <div className="d-flex justify-content-end">
-            <a href="/sap-business-one" className="mt-4 mt-md-0">
-              Learn More <span>&#8594;</span>
-            </a>
+          <div className="ps-lg-5 mt-4 mt-lg-0">
+            <div className="ps-lg-5">
+              <p className="sap-description">
+                SAP Business One is a comprehensive, integrated ERP solution
+                designed to streamline operations, improve decision-making, and
+                drive growth for small and medium-sized businesses. Whether
+                you're managing inventory, finances, or production, SAP Business
+                One provides the tools you need to take your business to the
+                next level.
+              </p>
+
+              <div className="d-flex align-items-center">
+                <img className="b1Img" src={sapB1} alt="sapb1" />
+              </div>
+            </div>
+
+            <div className="d-flex w-100 justify-content-end mt-lg-5">
+              <div className="d-flex justify-content-end">
+                <a href="/sap-business-one" className="mt-4 mt-md-0">
+                  Learn More <span>&#8594;</span>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/components/pages/home/contents/SapS4Hana.jsx
+++ src/components/pages/home/contents/SapS4Hana.jsx
@@ -1,5 +1,6 @@
 import "../../../../styles/pages/home/contents/SapS4Hana.css";
 import sapB1 from "../../../../assets/images/home/sapS4.png";
+import b1Img from "../../../../assets/images/sap/s4Home.png";
 
 export const SapS4Hana = () => {
   return (
@@ -8,26 +9,37 @@ export const SapS4Hana = () => {
         <p className="mb-0 info-tag">SAP S/4HANA PUBLIC CLOUD</p>
         <h3 className="main-t-all">THE INTELLIGENT ERP FOR THE DIGITAL AGE</h3>
 
-        <p className="sap-description">
-          SAP S/4HANA is an intelligent, next-generation ERP suite designed to
-          help large enterprises and fast-growing businesses drive digital
-          transformation. Built on an advanced in-memory database, SAP S/4HANA
-          enables real-time analytics, streamlined business processes, and
-          enhanced automation powered by AI and machine learning. Whether
-          managing finance, supply chain, manufacturing, or customer experience,
-          SAP S/4HANA delivers the agility, scalability, and innovation needed
-          to thrive in the digital economy.
-        </p>
-
-        <div className="d-flex w-100 flex-column align-items-between flex-md-row justify-content-md-between align-items-md-end mt-4">
-          <div className="d-flex align-items-center">
-            <img className="b1Img-s4" src={sapB1} alt="sapb1" />
+        <div className="d-lg-flex mt-4 pt-2">
+          <div className="d-flex justify-content-center justify-cotent-lg-start">
+            <img src={b1Img} className="s4ImgHome" alt="" />
           </div>
 
-          <div className="d-flex justify-content-end">
-            <a href="/sap-s4-hana" className="mt-4 mt-md-0">
-              Learn More <span>&#8594;</span>
-            </a>
+          <div className="ps-lg-5 mt-4 mt-lg-0">
+            <div className="ps-lg-5">
+              <p className="sap-description">
+                SAP S/4HANA is an intelligent, next-generation ERP suite
+                designed to help large enterprises and fast-growing businesses
+                drive digital transformation. Built on an advanced in-memory
+                database, SAP S/4HANA enables real-time analytics, streamlined
+                business processes, and enhanced automation powered by AI and
+                machine learning. Whether managing finance, supply chain,
+                manufacturing, or customer experience, SAP S/4HANA delivers the
+                agility, scalability, and innovation needed to thrive in the
+                digital economy.
+              </p>
+
+              <div className="d-flex align-items-center">
+                <img className="b1Img" src={sapB1} alt="sapb1" />
+              </div>
+            </div>
+
+            <div className="d-flex w-100 justify-content-end mt-lg-5">
+              <div className="d-flex justify-content-end">
+                <a href="/sap-s4-hana" className="mt-4 mt-md-0">
+                  Learn More <span>&#8594;</span>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
